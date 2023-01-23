294b6c8b832e6720df54ffe0bdba74db8ef8037e
erik-gustafsson <erikephggustafsson@gmail.com>
2023-01-23 16:21:18 +0100

    quick fix
    Added: hashmap char to char in translate.jsm

--- lost-in-translation/src/Views/Translate.jsx
+++ lost-in-translation/src/Views/Translate.jsx
@@ -1,15 +1,32 @@
 const Translate = () => {
+  const signMap = new Map();
+  signMap.set("a", 1);
+  signMap.set("b", 2);
+  signMap.set("c", 3);
+
+  const translateStr = "abc";
+  
+  const translateString = (str) =>{  
+    let rStr = ""
+    for(let i = 0; i < str.length; i++) {
+     rStr += signMap.get(str.charAt(i));   
+    }
+    return rStr;
+  }
+
   return (
     <>
-      <h1>Translate</h1>
+      <h1>Translate</h1> 
+      <h2>{translateString(translateStr)}</h2>
+
       {
         //to do: headline
         //Input field
         //(hash)map char -> hand
         //output button
-            //translate text
-            //add history
-                //remove old if over 10
+        //translate text
+        //add history
+        //remove old if over 10
       }
     </>
   );
